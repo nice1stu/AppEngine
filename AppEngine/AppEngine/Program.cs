42c784b3f7566945a1442634574e03ceb18e443a
nice1stu <stewart.wan@student.forsbergsskola.se>
2022-11-15 15:49:38 +0100

    Update Program.cs

--- AppEngine/AppEngine/Program.cs
+++ AppEngine/AppEngine/Program.cs
@@ -9,6 +9,7 @@ Glfw.WindowHint(Hint.ContextVersionMajor, 3);
 Glfw.WindowHint(Hint.ContextVersionMinor, 3);
 Glfw.WindowHint(Hint.OpenglProfile, Profile.Core);
 Glfw.WindowHint(Hint.OpenglForwardCompatible, Constants.True);
+Glfw.WindowHint(Hint.Doublebuffer, Constants.True);
 
 Window window = Glfw.CreateWindow(800, 600, "AppEngine", Monitor.None, Window.None);
 
@@ -16,7 +17,18 @@ Glfw.MakeContextCurrent(window);
 
 Import(Glfw.GetProcAddress);
 
-while (true)
+while (!Glfw.WindowShouldClose(window))
 {
+    // update input
     Glfw.PollEvents();
+    
+    // update your game
+    
+    // render
+    glClearColor(.2f, .05f,.2f, 1);
+    glClear(GL_COLOR_BUFFER_BIT);
+    //glFlush();
+    Glfw.SwapBuffers(window);
 }
+
+Glfw.Terminate();
