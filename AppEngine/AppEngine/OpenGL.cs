f62de3ecec42ace3aadec77716ad317e7fae8108
nice1stu <stewart.wan@student.forsbergsskola.se>
2022-11-15 15:35:17 +0100

    set up window

--- AppEngine/AppEngine/OpenGL.cs
+++ AppEngine/AppEngine/OpenGL.cs
@@ -3,7 +3,6 @@ using System.Linq;
 using System.Runtime.InteropServices;
 using System.Security;
 using System.Text;
-using JetBrains.Annotations;
 
 namespace OpenGL
 {
--- AppEngine/AppEngine/Program.cs
+++ AppEngine/AppEngine/Program.cs
@@ -1,3 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using System;
+using GLFW;
+using static OpenGL.Gl;
 
-Console.WriteLine("Hello, World!");
+Console.WriteLine("Starting engine...");
+Glfw.Init();
+Glfw.WindowHint(Hint.ClientApi, ClientApi.OpenGL);
+Glfw.WindowHint(Hint.ContextVersionMajor, 3);
+Glfw.WindowHint(Hint.ContextVersionMinor, 3);
+Glfw.WindowHint(Hint.OpenglProfile, Profile.Core);
+Glfw.WindowHint(Hint.OpenglForwardCompatible, Constants.True);
+
+Window window = Glfw.CreateWindow(800, 600, "AppEngine", Monitor.None, Window.None);
+
+Glfw.MakeContextCurrent(window);
+
+Import(Glfw.GetProcAddress);
+
+while (true)
+{
+    Glfw.PollEvents();
+}
