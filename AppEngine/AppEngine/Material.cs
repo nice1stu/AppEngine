d859b5195a59f68cadf33b2f9690ebdfd54b57b1
nice1stu <stewart.wan@student.forsbergsskola.se>
2022-11-16 23:49:28 +0100

    change triangle color to blue

--- AppEngine/AppEngine/Material.cs
+++ AppEngine/AppEngine/Material.cs
@@ -42,7 +42,7 @@ public class Material
 
         void main()
         {
-            color = vec4(1.0f, 0.5f, 0.2f, 1.0f);
+            color = vec4(0.0f, 0.0f, 1.0f, 1.0f);
         }
         ";
 
