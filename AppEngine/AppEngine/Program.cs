9f2210b4c871a97f5c9ed8c5e14d280123bb94f0
nice1stu <stewart.wan@student.forsbergsskola.se>
2022-11-16 13:56:05 +0100

    got triangle

--- AppEngine/AppEngine/Program.cs
+++ AppEngine/AppEngine/Program.cs
@@ -25,6 +25,47 @@ void OnKeyCallback(IntPtr window, Keys key, int scanCode, InputState state, Modi
     }
 }
 
+// create vertex shader GLSL
+const string vertexShaderCode = @"
+#version 330 core
+
+layout (location = 0) in vec3 position;
+
+void main()
+{
+    gl_position = vec4(position.x, position.y, position.z, 1.0);
+";
+
+uint vertexShader = glCreateShader(GL_VERTEX_SHADER);
+glShaderSource(vertexShader, vertexShaderCode);
+glCompileShader(vertexShader);
+
+// create fragment shader (pixel shader)
+const string fragmentShaderCode = @"
+#version 330 core
+
+out vec4 color;
+
+void main()
+{
+    color = vec4(1.0f, 0.5f, 0.2f, 1.0f);
+}
+";
+
+uint fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+glShaderSource(fragmentShader, fragmentShaderCode);
+glCompileShader(fragmentShader);
+
+// create & use render pipeline
+uint shaderProgram = glCreateProgram();
+
+// Todo: attach shaders
+glAttachShader(shaderProgram, vertexShader);
+glAttachShader(shaderProgram, fragmentShader);
+
+glLinkProgram(shaderProgram);
+glUseProgram(shaderProgram);
+
 float[] vertices =
 {
     -.5f, -.5f, 0f,
@@ -32,8 +73,8 @@ float[] vertices =
     0f, +.5f, 0f
 };
 // create & use a cache for vertex buffer
-uint vertexarrayObject = glGenVertexArray();
-glBindVertexArray(vertexarrayObject);
+uint vertexArrayObject = glGenVertexArray();
+glBindVertexArray(vertexArrayObject);
 // create & use a buffer for vertex array data
 uint vertexBufferObject = glGenBuffer();
 glBindBuffer(GL_ARRAY_BUFFER, vertexBufferObject);
@@ -45,6 +86,8 @@ unsafe
         glBufferData(GL_ARRAY_BUFFER, sizeof(float)*vertices.Length, vertex, GL_STATIC_DRAW);
     }
 }
+glVertexAttribPointer(0, 3, GL_FLOAT, false, 3 * sizeof(float), IntPtr.Zero);
+glEnableVertexAttribArray(0);
 
 while (!Glfw.WindowShouldClose(window))
 {
