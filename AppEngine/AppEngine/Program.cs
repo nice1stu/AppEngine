7de2eae8d8293a40ddc32157ece6fa8a9ded7552
nice1stu <stewart.wan@student.forsbergsskola.se>
2022-11-17 15:41:42 +0100

    add deltaTime

--- AppEngine/AppEngine/Program.cs
+++ AppEngine/AppEngine/Program.cs
@@ -12,18 +12,22 @@ Material material = new Material();
 material.Use();
 MeshRenderer triangle = new MeshRenderer();
 
+float lastFrameTime = (float)Glfw.Time;
 while (!window.ShouldClose)
 {
+    float deltaTime = (float)Glfw.Time - lastFrameTime;
+    lastFrameTime = (float)Glfw.Time;
+    
     Vector movement = Vector.Zero;
     if (window.GetKey(Keys.W))
-        movement.Y += 0.01f;
+        movement.Y += 1f;
     if (window.GetKey(Keys.S))
-        movement.Y += -0.01f;
+        movement.Y += -1f;
     if (window.GetKey(Keys.A))
-        movement.X += -0.01f;
+        movement.X += -1f;
     if (window.GetKey(Keys.D))
-        movement.X += 0.01f;
-    triangle.Position = triangle.Position.Add(movement);
+        movement.X += 1f;
+    triangle.Position = triangle.Position.Add(movement.MultiplyWith(deltaTime));
 
     window.BeginRender();
     triangle.Render();
