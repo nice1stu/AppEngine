8a55f5088405897f6b07b00b34a40b7abe8fce0b
nice1stu <stewart.wan@student.forsbergsskola.se>
2022-11-23 16:30:58 +0100

    Update MeshRenderer.cs

--- AppEngine/AppEngine/MeshRenderer.cs
+++ AppEngine/AppEngine/MeshRenderer.cs
@@ -14,27 +14,35 @@ public class MeshRenderer
         new Vertex(new Vector(0.5f, 0.5f, 0.5f), Color.White, 1f, 1f), // 2: top-right
         new Vertex(new Vector(0.5f, -0.5f, 0.5f), Color.White, 1f ,0f), // 3: bottom-right
         
-        new Vertex(new Vector(-0.5f, -0.5f, -0.5f), Color.White, 0f,0f), // 0: bottom-left
-        new Vertex(new Vector(-0.5f,0.5f, -0.5f), Color.White, 0f,1f), // 1: top-left
-        new Vertex(new Vector(0.5f, 0.5f, -0.5f), Color.White, 1f, 1f), // 2: top-right
-        new Vertex(new Vector(0.5f, -0.5f, -0.5f), Color.White, 1f ,0f), // 3: bottom-right
+        new Vertex(new Vector(-0.5f, -0.5f, -0.5f), Color.White, 1f,0f), // 4: bottom-left
+        new Vertex(new Vector(-0.5f,0.5f, -0.5f), Color.White, 1f,1f), // 5: top-left
+        new Vertex(new Vector(0.5f, 0.5f, -0.5f), Color.White, 0f, 1f), // 6: top-right
+        new Vertex(new Vector(0.5f, -0.5f, -0.5f), Color.White, 0f ,0f), // 7: bottom-right
+        
+        new Vertex(new Vector(-0.5f,0.5f, 0.5f), Color.White, 0f,0f), // 8: top-left
+        new Vertex(new Vector(0.5f, 0.5f, 0.5f), Color.White, 1f, 0f), // 9: top-right
+        new Vertex(new Vector(0.5f,0.5f, -0.5f), Color.White, 1f,1f), // 10: top-right back
+        new Vertex(new Vector(-0.5f, 0.5f, -0.5f), Color.White, 0f, 1f), // 11: top-left back
     };
 
     private static readonly uint[] indices =
     {
+        //front
         0, 1, 2,
         0, 3, 2,
+        //back
         7, 6, 5,
         7, 4, 5,
-     
+        //bottom
         0, 3, 4,
         3, 4, 7,
-        1, 2, 6,
-        1, 5, 6,
-        
+        //top
+        11, 8, 9,
+        11, 10, 9,
+        //right
         0, 1, 5,
         0, 4, 5,
-        
+        //side
         2, 3, 6,
         3, 6, 7
     };
